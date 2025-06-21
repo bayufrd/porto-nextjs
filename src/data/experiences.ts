5ab615009533fcd1a7d38f535f8d8cc271dc6269
bayufrd <55101246+bayufrd@users.noreply.github.com>
2025-06-21 07:06:53 +0700

    [UPDATE] Editing Timelines experience

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,5 +1,43 @@
 const experiences = [
   {
+    designation: "Project Manager",
+    company: "Akhari Tech & IT Consultant",
+    startDate: "January 2023",
+    endDate: "January 2024",
+    isCurrentJob: false,
+    location: "Remote, Yogyakarta, Indonesia",
+    description: [
+      "Proposed digital solutions to resolve client issues effectively.",
+      "Negotiated agreements to meet mutual client goals and oversaw project timelines.",
+      "Acted as the liaison between clients and developers to ensure smooth communication.",
+    ],
+  }, {
+    designation: "Fullstack Developer dotNet C#",
+    company: "PT Mitra Transformasi Digital",
+    staÍrtDate: "January 2024",
+    endDate: "March 2024",
+    isCurrentJob: false,
+    location: "Remote, Semarang, Indonesia",
+    description: [
+      "Intern C# building and developing a CMS login page using the .NET framework.",
+      "Designed and implemented backend architectures to enhance project performance and user experience.",
+    ],
+  },
+
+  {
+    designation: "Software Developer dotNet C#",
+    company: "PT. Gaspol Management Center",
+    startDate: "April 2024",
+    endDate: "May 2024",
+    isCurrentJob: true,
+    location: "Yogyakarta, Indonesia",
+    description: [
+      "Interned as a Software Developer and IT Consultant focusing on a Point-of-Sale application.",
+      "Developed a membership management system with online monitoring capabilities integrated into the company’s website.",
+      "Implemented user-friendly UI/UX designs for applications focused on customer data management.",
+    ],
+  },
+  {
     designation: "Full-Stack Web Developer Next and Node",
     company: "CV. Sentra Teknologi Investama",
     startDate: "March 2024",
@@ -12,44 +50,6 @@ const experiences = [
       "Built a responsive, SEO-friendly user interface ensuring fast load times.",
       "Implemented a CRUD system for easy upload and management of product data through the CMS.",
       "Ensured site responsiveness and optimized performance with best practices.",
-    ],
-  },
-  {
-    designation: "Fullstack Developer dotNet C#",
-    company: "PT Mitra Transformasi Digital",
-    startDate: "January 2024",
-    endDate: "March 2024",
-    isCurrentJob: false,
-    location: "Remote, Semarang, Indonesia",
-    description: [
-      "Intern C# building and developing a CMS login page using the .NET framework.",
-      "Designed and implemented backend architectures to enhance project performance and user experience.",
-    ],
-  },
-  {
-    designation: "Project Manager",
-    company: "Akhari Tech & IT Consultant",
-    startDate: "January 2023",
-    endDate: "January 2024",
-    isCurrentJob: false,
-    location: "Remote, Yogyakarta, Indonesia",
-    description: [
-      "Proposed digital solutions to resolve client issues effectively.",
-      "Negotiated agreements to meet mutual client goals and oversaw project timelines.",
-      "Acted as the liaison between clients and developers to ensure smooth communication.",
-    ],
-  },
-  {
-    designation: "Software Developer dotNet C#",
-    company: "PT. Gaspol Management Center",
-    startDate: "April 2024",
-    endDate: "May 2024",
-    isCurrentJob: true,
-    location: "Yogyakarta, Indonesia",
-    description: [
-      "Interned as a Software Developer and IT Consultant focusing on a Point-of-Sale application.",
-      "Developed a membership management system with online monitoring capabilities integrated into the company’s website.",
-      "Implemented user-friendly UI/UX designs for applications focused on customer data management.",
     ],
   },
 ];
